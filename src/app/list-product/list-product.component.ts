3ad03167d28a013f7142a0573811cea8cbbb6d53
Skander <msalmi.skander@esprit.tn>
2022-10-04 09:50:22 +0100

    databinding twoway

--- src/app/list-product/list-product.component.ts
+++ src/app/list-product/list-product.component.ts
@@ -9,6 +9,7 @@ import { Product } from '../model/product';
 export class ListProductComponent implements OnInit {
   public title: String;
   public list:Product[];
+  public priceMax:number=12;
   constructor() {
   }
   ngOnInit(): void {
