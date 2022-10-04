70bb367d04b3e13535468bfb3a378c82ebb9d0a2
Skander <msalmi.skander@esprit.tn>
2022-10-04 11:02:14 +0100

    router link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,23 @@ import { ListProductComponent } from './list-product/list-product.component';
 import { FooterComponent } from './footer/footer.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { Routes , RouterModule} from '@angular/router';
+const ROUTES: Routes = [
+  {
+    path:'home',component: HomeComponent
+  },
+  {
+    path:'products',component:ListProductComponent
+  },
+  {
+    path:'contact',component:ContactComponent
+  },
+  {
+    path:'users',component:UserListComponent
+  }
+]
 /*.In our AppModule, we then need to specify the component that will serve as the entry point component for our application.
 In our app.module.ts file where we import the entry component (conventionally AppComponent) and supply it as the only item
 in our bootstrap array inside the NgModule configuration object. eg. bootstrap[AppComponent]*/
@@ -16,11 +33,14 @@ in our bootstrap array inside the NgModule configuration object. eg. bootstrap[A
     HeaderComponent,
     ListProductComponent,
     FooterComponent,
-    UserListComponent
+    UserListComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
